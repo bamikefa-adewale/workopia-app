3f403aa955203404995d4323368234accf0a87a2
bamikefa-adewale <bamikefaadewale@gmail.com>
2025-01-16 16:15:02 +0100

    i implememted save apply job button so job owner will not be able to see apply job

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -93,12 +93,15 @@ const JobDetails = () => {
           Put Job Application as the subject of your email and attach your
           resume.
         </p>
-        <a
-          href="mailto:manager@company.com"
-          className="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
-        >
-          Apply Now
-        </a>
+        {!isJobOwner && (
+          <button
+            href="mailto:manager@company.com"
+            disabled={isPending}
+            className="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+          >
+            {isPending ? "Applying..." : " Apply Now"}
+          </button>
+        )}
       </section>
 
       <BottomBanner />
