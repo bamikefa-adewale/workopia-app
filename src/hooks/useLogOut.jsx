b8db31bf1c00794f661e42a74a82b4d317071300
bamikefa-adewale <bamikefaadewale@gmail.com>
2025-02-08 16:34:02 +0100

    redirect to home page after user successfully logout

--- src/hooks/useLogOut.jsx
+++ src/hooks/useLogOut.jsx
@@ -2,16 +2,20 @@ import { useMutation } from "@tanstack/react-query";
 import { LogOutCurrentUser } from "../Service/Auth";
 import { toast } from "react-toastify";
 import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useLogOut = () => {
+  const naviagate = useNavigate();
   const { setAuth } = useAuth();
   const { isPending, mutate } = useMutation({
     mutationFn: LogOutCurrentUser,
     onSuccess: (data) => {
       if (data?.logout) {
         toast.success(data?.message);
+
         setAuth(null);
         localStorage.clear();
+        naviagate("/");
       }
     },
   });
