fbd1f516be1bb727a5815b223bcc08ba2969ea35
bamikefa-adewale <bamikefaadewale@gmail.com>
2025-03-12 15:12:05 +0100

    fix auth issue

--- src/componenets/ui/CustomInput.jsx
+++ src/componenets/ui/CustomInput.jsx
@@ -1,22 +1,10 @@
 /* eslint-disable react/prop-types */
 
-const CustomInput = ({
-  placeholder,
-  type = "text",
-  register,
-  className = "",
-  error,
-  name,
-  label,
-  ...props
-}) => {
+const CustomInput = ({ register, className = "", error, label, ...props }) => {
   return (
     <>
       <label className="block text-gray-700 mb-2 font-bold">{label}</label>
       <input
-        type={type}
-        placeholder={placeholder}
-        name={name}
         {...register}
         {...props}
         className={`border rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
